b47dd0fbc4c4c5c62f103f3bac27f2b92d170cab
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2023-11-20 21:00:24 +0100

    correct

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,6 @@
-// Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
-// Доповни код функції таким чином, що:
-// Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
-// Якщо в рядку відсутні заборонені слова, функція повертає буль false
-
 function checkForSpam(message) {
-    const lowerCaseMessage = message.toLowerCase();
-    if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
-        return true;
-    }
-    else {
-        return false;
-    }
+    const normalizedMessage = message.toLowerCase();
+    return normalizedMessage.includes('spam') || normalizedMessage.includes('sale');
 }
 
 console.log(checkForSpam("Latest technology news")); // false
--- js/task-4.js
+++ js/task-4.js
@@ -1,29 +1,20 @@
-// Оголоси функцію getShippingCost(country), яка повинна перевіряти можливість доставки товару в країну користувача (параметр country) і повертати повідомлення про результат. Обов'язково використовуй інструкцію switch.
-// Формат рядка, що повертається "Shipping to <country> will cost <price> credits", де замість <country> і <price> необхідно підставити відповідні значення.
-// Список країн і вартість доставки:
-// China — 100 кредитів
-// Chile — 250 кредитів
-// Australia — 170 кредитів
-// Jamaica — 120 кредитів
-// Зі списку видно, що доставка можлива не скрізь. Якщо зазначена країна відсутня у списку, то функція повинна повернути рядок "Sorry, there is no delivery to your country".
-
 function getShippingCost(country) {
    let price;
    switch (country) {
       case "China":
-         price = "100";
+         price = 100;
          break;
       case "Chile":
-         price = "250";
+         price = 250;
          break;
       case "Australia":
-         price = "170";
+         price = 170;
          break;
       case "Jamaica":
-         price = "120";
+         price = 120;
          break;     
       default:
-         price = "Sorry, there is no delivery to your country."         
+         "Sorry, there is no delivery to your country."         
    }
    return `Shipping to ${country} will cost ${price} credits`;
 }
